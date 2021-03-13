37ec3982184e0803fe438dd7bbfd49700e556d34
Martin Hettiger <martin@hettiger.com>
2021-03-13 17:46:52 +0100

    Refactor EmojiMemoryGameThemeStore

--- Memorize/User Interface/View Models/EmojiMemoryGameThemeStore.swift
+++ Memorize/User Interface/View Models/EmojiMemoryGameThemeStore.swift
@@ -14,19 +14,28 @@ class EmojiMemoryGameThemeStore: ObservableObject {
     private let userDefaults: UserDefaults
 
     @Published
-    var themes: [Game.Theme]
+    var themes = [Game.Theme]()
 
     private var subscriptions = [AnyCancellable]()
 
     init(userDefaults: UserDefaults) {
         self.userDefaults = userDefaults
 
-        let json = userDefaults.string(forKey: UserDefaults.Key.themes.rawValue)
-        let data = json?.data(using: .utf8)
-        let themes = try? JSONDecoder().decode([Game.Theme].self, from: data ?? .init())
-        self.themes = themes ?? .init()
+        fetchThemes()
+        autosaveThemes()
+    }
 
-        subscriptions.append($themes.sink { themes in
+    private func fetchThemes() {
+        guard
+            let json = userDefaults.string(forKey: UserDefaults.Key.themes.rawValue),
+            let data = json.data(using: .utf8),
+            let themes = try? JSONDecoder().decode([Game.Theme].self, from: data)
+        else { return }
+        self.themes = themes
+    }
+
+    private func autosaveThemes() {
+        subscriptions.append($themes.sink { [userDefaults] themes in
             let data = (try? JSONEncoder().encode(themes)) ?? .init()
             let json = String(data: data, encoding: .utf8) ?? nil
             userDefaults.setValue(json, forKey: UserDefaults.Key.themes.rawValue)
